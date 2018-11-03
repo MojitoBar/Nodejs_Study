f51a9b1ebce5e6c35428bc99b72c38a4978dc809
MojitoBar <wnehdtjr5@naver.com>
2018-11-04 00:24:58 +0900

    study route & static, dynamic

--- app_file.js
+++ app_file.js
@@ -1,8 +1,34 @@
 var express = require('express');
+
 var app = express();
+
+// 정적인 파일이 위치할 디렉토리 정하기
+app.use(express.static('public'));
+
+// get = 일종의 라우팅 설정
 app.get('/', function(req, res){
     res.send('Hello home page');
 });
+app.get('/login', function(req, res){
+    res.send('Please Login');
+})
+app.get('/route', function(req, res){
+    res.send('Hello route, <img src="/route.png">')
+})
+app.get('/dynamic', function(req, res){
+    var output = `
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset = "utf-8">
+        </head>
+        <body>
+            Hello, Dynamic!
+        </body>
+    </html>
+    `;
+    res.send(output)
+})
 app.listen(3000, function(){
     console.log('Connected 3000 port!');
 });
