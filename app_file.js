c33d1d4893c924740cb33b24b8a3c9daad0ff5b9
MojitoBar <wnehdtjr5@naver.com>
2018-11-04 01:04:16 +0900

    study Jade

--- app_file.js
+++ app_file.js
@@ -1,6 +1,11 @@
 var express = require('express');
-
 var app = express();
+
+// 뷰 엔진(템플릿)인 jade를 추가하겠다.
+app.set('view engine', 'jade');
+
+// 디폴트값이 views  
+app.set('views', './views')
 
 // 정적인 파일이 위치할 디렉토리 정하기
 app.use(express.static('public'));
@@ -9,6 +14,10 @@ app.use(express.static('public'));
 app.get('/', function(req, res){
     res.send('Hello home page');
 });
+app.get('/template', function(req, res){
+    // render = views 안에 있는 temp 라는 jade 파일을 보여주겠다.
+    res.render('temp', {time: 'hello', _title: 'Jade'});
+})
 app.get('/login', function(req, res){
     res.send('Please Login');
 })
@@ -16,6 +25,10 @@ app.get('/route', function(req, res){
     res.send('Hello route, <img src="/route.png">')
 })
 app.get('/dynamic', function(req, res){
+    var lis = '';
+    for(var i=0; i<5; i++){
+        lis = lis + '<li>coding</li>';
+    }
     var output = `
     <!DOCTYPE html>
     <html>
@@ -24,6 +37,7 @@ app.get('/dynamic', function(req, res){
         </head>
         <body>
             Hello, Dynamic!
+            ${lis}
         </body>
     </html>
     `;
