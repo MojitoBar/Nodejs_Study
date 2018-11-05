70a4400a46986d4431c512a9bf6c703335355c7f
MojitoBar <wnehdtjr5@naver.com>
2018-11-05 14:12:17 +0900

    nodejs 로 mysql INSERT

---
+++ database_mysql.js
@@ -0,0 +1,33 @@
+var mysql = require('mysql');
+var conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '111111',
+    database: 'o2'
+});
+conn.connect();
+
+// var sql = 'select * from topic';
+// conn.query(sql, function(err, rows, fields){
+//     if (err){
+//         console.log(err);
+//     }
+//     else{
+//         for(var i=0; i<rows.length; i++){
+//             console.log(rows[i].author);
+//         }
+//     }
+// });
+
+// var sql = 'INSERT INTO topic (title, description, author) VALUES(?, ?, ?)';
+// var params = ['Supervisor', 'Watcher', 'graphittie'];
+// conn.query(sql, params, function(err, rows, fields){
+//     if(err){
+//         console.log(err);
+//     }
+//     else{
+//         console.log(rows);
+//     }
+// })
+
+conn.end();
