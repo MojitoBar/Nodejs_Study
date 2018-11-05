0a6538213ae9113715a7cced0b80697350e707d0
MojitoBar <wnehdtjr5@naver.com>
2018-11-05 15:03:39 +0900

    Change fs to database

---
+++ app_mysql.js
@@ -0,0 +1,83 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var fs = require('fs')
+var app = express();
+
+var mysql = require('mysql');
+var conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '111111',
+    database: 'o2'
+});
+conn.connect();
+
+
+// post 방식으로 들어오는 값을 처리하는 녀석
+app.use(bodyParser.urlencoded({ extended: false }));
+app.set('views', './views_mysql');
+app.set('view engine', 'jade');
+app.get('/topic/new', function (req, res) {
+    fs.readdir('data', function (err, files) {
+        if(err) {
+            console.log(err);
+            res.status(500).send('Internal Server Error');
+        }    
+        res.render('new', {topics: files});
+    });
+})
+app.get(['/topic', '/topic/:id'], function (req, res) {
+    var sql = 'SELECT id, title FROM topic';
+    conn.query(sql, function(err, topics, fields){
+        var id = req.params.id;
+        if(id){
+            var sql = 'SELECT * FROM topic WHERE id=?';
+            conn.query(sql, [id], function(err, topic, fields){
+                if(err){
+                    console.log(err);
+                }
+                else{
+                    res.render('view', { topics: topics, topic: topic[0] }); 
+                }
+            });
+        }
+        else{
+            res.render('view', { topics: topics });             
+        }
+    });
+    // fs.readdir('data', function (err, files) {
+    //     if(err) {
+    //         console.log(err);
+    //         res.status(500).send('Internal Server Error');
+    //     }
+    //     var id = req.params.id;
+    //     if(id) {
+    //         // id 값이 있다면
+    //         fs.readFile('data/' + id, 'utf8', function (err, data) {
+    //             if (err) {
+    //                 console.log(err);
+    //                 res.status(500).send('Internal Server Error');
+    //             }
+    //             res.render('view', { topics: files, title: id, description: data });
+    //         })
+    //     }
+    //     else{
+    //         // id 값이 없다면
+    //         res.render('view', { topics: files, title: 'Welcome', description: 'Hello, Javascript for server.'});
+    //     }
+    // })
+})
+
+app.post('/topic', function (req, res) {
+    var title = req.body.title;
+    var description = req.body.description;
+    fs.writeFile('data/' + title, description, function (err) {
+        if (err) {
+            res.status(500).send('Internal Server Error');
+        }
+        res.redirect('/topic/'+title);
+    });
+})
+app.listen(3000, function (req, res) {
+    console.log('Connected, 3000 port!');
+})
--- database_mysql.js
+++ database_mysql.js
@@ -30,4 +30,15 @@ conn.connect();
 //     }
 // })
 
+var sql = 'DELETE FROM topic WHERE id=?';
+var params = [1];
+conn.query(sql, params, function(err, rows, fields){
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log(rows);
+    }
+})
+
 conn.end();
